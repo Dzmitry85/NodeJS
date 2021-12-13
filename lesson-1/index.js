bac26d57c32787b98e5ef8c4d7a9666ff2fa8186
Dzmitry85 <dpinchuk85@gmail.com>
2021-12-13 19:54:17 +0300

    Практическое задание-3

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,56 +1,29 @@
+const fs = require('fs');
+const readline = require('readline')
 
-require('colors')
 
-const colors = {GREEN : 0, YELLOW: 1, RED : 2}
+const readFile = fs.createReadStream('./access.log', 'utf8');
 
-let colorCurrent = colors.GREEN;
+const data = process.argv[2];
+const recordFile = fs.createWriteStream(`${data}` + '_requests.log')
 
-const n = process.argv[2];
 
-const changeColor = () => {
-    colorCurrent++;
-    if (colorCurrent > colors.RED)
-        colorCurrent = colors.GREEN;
+
+const rl = readline.createInterface({
+    input: readFile,
+
+
+});
+
+rl.on('line', (line) => {
+
+
+    if (line.includes(data)) {
+           recordFile.write(line + "\n")
+        console.log('чтение данных')
+
 }
-
-const colorPrint = (i) => {
-
-    switch (colorCurrent){
-        case colors.RED:
-            console.log(`${i}`.red);
-            break;
-        case colors.GREEN:
-            console.log(`${i}`.green);
-            break;
-        case colors.YELLOW:
-            console.log(`${i}`.yellow);
-            break;
-    }
-    changeColor();
-}
-if( isNaN(n)){
-    console.log('ERROR, incorrect  parameters'.red);
-    return;
-}
-
-    if (n <= 1)
-        return console.log(`ERROR, parameters <=1  `.red) ;
-
-    for (let i = 2; i <= n; i++) {
-        let flag = 1;
-        if (i > 2 && i % 2 != 0) {
-            for (let j = 3; j * j <= i; j = j + 2) {
-                if (i % j == 0) {
-                    flag = 0;
-                    break;
-                }
-            }
-        } else if (i != 2) flag = 0;
-        if (flag == 1) {
-            colorPrint(i);
-
-    }}
-
+ });
 
 
 
